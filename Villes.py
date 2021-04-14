cc90e61d1b15c51701c8dfd2ac35e94b49472195
Teddy <metoyerteddy@gmail.com>
2021-04-14 13:50:59 +0200

    Add method for check if city exist

--- Villes.py
+++ Villes.py
@@ -4,6 +4,11 @@ from Ville import Ville
 class Villes:
     villes = {}
 
+    # Vérifie si la ville existe
+    @staticmethod
+    def villeExist(name):
+        return str(name).lower() in Villes.villes
+
     # Récupére l'instance de la class Ville par le nom dans le dictionaire
     @staticmethod
     def getVilleByName(villeName):
@@ -12,11 +17,13 @@ class Villes:
             return Villes.villes[villeName]
         return
 
+    # Ajoute une ville
     @staticmethod
     def addVille(ville: Ville):
         name = str.lower(ville.nom)
         Villes.villes[name] = ville
 
+    # Ajoute une liste de villes
     @staticmethod
     def addVilles(villes: [Ville]):
         for ville in villes:

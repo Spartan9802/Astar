2b5dd2112baa33467a0be82b5e32324efd2534f8
Teddy <metoyerteddy@gmail.com>
2021-04-29 17:53:47 +0200

    Add geocodes api

--- api.py
+++ api.py
@@ -1,11 +1,16 @@
+import json
 import main
 from flask import Flask, jsonify
 from flask_cors import CORS
 from Villes import Villes
+import requests
 
+global villes
 
+hereApi = 'jwdvUmFcg-KUIdCUxcs7doiBx03uipAbTsgvfr-VNT0'
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 
 @app.route('/api/<name>/')
 def getVilleByName(name):
@@ -29,13 +34,31 @@ def routeBetween(ville1, ville2):
         return jsonify({'error': 'Route introuvable'})
     return jsonify({'error': 'Une des villes n\'existe pas'})
 
+
 @app.route('/api/exist/<ville>')
 def exist(ville):
     return jsonify({'status': Villes.villeExist(ville)})
 
 
+def getGeocode(address):
+    url = "https://geocoder.ls.hereapi.com/search/6.2/geocode.json?languages=fr-FR&maxresults=1&searchtext=" + address + "&apiKey=" + hereApi
+    response = requests.get(url)
+    json = response.json()
+    return json['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']
 
 
+def loadGeocodes():
+    global villes
+    with open('./geocodes.json', 'r') as file:
+        villes = json.load(file)
+
+@app.route('/api/villes/')
+def getVilles():
+    global villes
+    return jsonify({'villes': villes})
+
+
+loadGeocodes()
 if __name__ == '__main__':
     app.config['JSON_AS_ASCII'] = False
     app.run(debug=True, host="0.0.0.0")
